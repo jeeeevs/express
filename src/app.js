4775ea149e37b640e84e0aff4157f84770e5551c
Jeevan N <jeevan.nazar@publicissapient.com>
2019-09-05 23:29:15 +0100

    fixing typo

--- src/app.js
+++ src/app.js
@@ -2,6 +2,6 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send({msg: 'Hello Workd'}));
+app.get('/', (req, res) => res.send({msg: 'Hello World'}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
